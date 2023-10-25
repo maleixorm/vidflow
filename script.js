3c58195bc9eaba1b571870b256ef936f8194e65d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-10-25 10:48:20 -0300

    Construindo a lógica da pesquisa

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 const containerVideos = document.querySelector('.videos__container');
 
 async function buscarEMostrarVideos() {
-    const busca = await fetch('http://localhost:3000/videos');
-    const videos = await busca.json();
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-        `;
-    })
+    try {
+        const busca = await fetch('http://localhost:3000/videos');
+        const videos = await busca.json();
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+            `;
+        })
+    } catch(error) {
+        containerVideos.innerHTML = `<p>Erro ao carregar os vídeos: ${error}</p>`;
+    }
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+barraDePesquisa.addEventListener('input', filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    if (barraDePesquisa.value != "") {
+        for(let video of videos) {
+            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = 'none';
+            } else {
+                video.style.display = 'block';
+            }
+        }
+    } else {
+        video.style.display = 'block';
+    }
+}
